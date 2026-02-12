75ad03067e2115addb92472ee3ad088b1bbc8c65
jorgelpat <156463233+jorgelpat@users.noreply.github.com>
2026-02-12 11:09:44 -0500

    :construction: :lipstick: se agrega transición y movimiento al sidebar

---
+++ js/dashboard.js
@@ -0,0 +1,7 @@
+const menu = document.getElementById('menu');
+const sidebar = document.getElementById('sidebar');
+
+menu.addEventListener('click', ()=>{
+    sidebar.classList.toggle('menu-toggle');
+    menu.classList.toggle('menu-toggle');
+});
